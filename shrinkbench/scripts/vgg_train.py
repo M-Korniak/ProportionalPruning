a76c66ab4cbc20bb971bf193caac009f7c83bb3d
Mikolaj Czarnecki <mikolajczarnecki235@gmail.com>
2025-04-12 22:05:07 +0200

    Add debug strings

--- shrinkbench/scripts/vgg_train.py
+++ shrinkbench/scripts/vgg_train.py
@@ -1,8 +1,9 @@
 import os
 from shrinkbench.experiment import TrainingExperiment
 
+print("Hello from vgg_train")
 os.environ['DATAPATH'] = '$HOME/ProportionalPruning/data/CIFAR10'
-
+print("Datapath env set")
 exp = TrainingExperiment(dataset='CIFAR10', 
                                 model='vgg11',
                                 pretrained=False,
